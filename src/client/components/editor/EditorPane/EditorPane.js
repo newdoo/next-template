8f80f21d270e761290604fe4642ac89d93f2529c
DESKTOP-71BE5MQ\doo <doo8866@numixent.com>
2018-10-04 10:35:05 +0900

    EditorPane CodeMirror 적용

--- src/client/components/editor/EditorPane/EditorPane.js
+++ src/client/components/editor/EditorPane/EditorPane.js
@@ -2,31 +2,57 @@ import React, { Component } from 'react';
 import styles from './EditorPane.scss';
 import classNames from 'classnames/bind';
 
+// CodeMirror를 위한 CSS 스타일
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+
+// 브라우저일 때만 로딩
+let CodeMirror = null;
+const isBrowser = process.browser;
+if(isBrowser) {
+  CodeMirror = require('codemirror');
+  require('codemirror/mode/markdown/markdown');
+  require('codemirror/mode/javascript/javascript');
+  require('codemirror/mode/jsx/jsx');
+  require('codemirror/mode/css/css');
+  require('codemirror/mode/shell/shell');
+}
+
 const cx = classNames.bind(styles);
 
 class EditorPane extends Component {
 
-  render() {
-    const { handleChange } = this;
-    const { tags, title } = this.props;
+  editor = null // 에디터 ref
+  codeMirror = null // CodeMirror 인스턴스
+  
+  initializeEditor = () => {
+    this.codeMirror = CodeMirror(this.editor, {
+      mode: 'markdown',
+      theme: 'monokai',
+      lineNumbers: true, // 좌측에 라인 넘버 띄우기
+      lineWrapping: true // 내용이 너무 길면 다음 줄에 작성
+    });
+  }
 
+  componentDidMount() {
+      this.initializeEditor();
+  }
+
+  render() {
+    
     return (
       <div className={cx('editor-pane')}>
         <input 
           className={cx('title')} 
           placeholder="제목을 입력하세요" 
           name="title"
-          value={title}
-          onChange={handleChange}
         />
-        <div className={cx('code-editor')}></div>
+        <div className={cx('code-editor')} ref={ref=>this.editor=ref}></div>
         <div className={cx('tags')}>
           <div className={cx('description')}>태그</div>
           <input 
             name="tags"
             placeholder="태그를 입력하세요 (쉼표로 구분)"
-            value={tags}
-            onChange={handleChange}
           />
         </div>
       </div>
--- src/client/next.config.js
+++ src/client/next.config.js
@@ -1,10 +1,20 @@
 const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
 const paths = require('./paths')
 
-module.exports = withSass({
+module.exports = withCSS(withSass({
   distDir: "../../.next",
   sassLoaderOptions: {
     includePaths: [paths.globalStyles]
+  },
+  /*
+  webpack: config => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty'
+    }
+
+    return config
   }
-  
-})
+  */
+}))
